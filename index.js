08ba2844a6cd1e1a36de2df82752d1d4c84252c1
Guy Blank <guy.blank@capriza.com>
2017-12-18 20:22:31 +0200

    add spy menu view

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
-import { NativeEventEmitter, NativeModules } from "react-native";
+import {
+  NativeEventEmitter,
+  NativeModules,
+  Modal,
+  View,
+  Text,
+  StyleSheet,
+  Switch,
+  Button
+} from "react-native";
 import MessageQueue from "react-native/Libraries/BatchedBridge/MessageQueue.js";
 
 const { RNDevToolsSpyEventEmitter } = NativeModules;
@@ -7,15 +16,20 @@ const { RNDevToolsSpyEventEmitter } = NativeModules;
 export default class DevToolsSpy extends Component {
   constructor(props) {
     super(props);
-    this.isSpyOn = false;
+    this.state = {
+      modalVisible: false,
+      isMessageQueueOn: false
+    };
+
+    this.onToggleMessageQueue = this.onToggleMessageQueue.bind(this);
+    this.onClose = this.onClose.bind(this);
   }
 
   componentWillMount() {
     this.subscription = new NativeEventEmitter(
       RNDevToolsSpyEventEmitter
     ).addListener("toggleSpy", () => {
-      this.isSpyOn = !this.isSpyOn;
-      MessageQueue.spy(this.isSpyOn);
+      this.setState({ modalVisible: !this.state.modalVisible });
     });
   }
 
@@ -23,7 +37,61 @@ export default class DevToolsSpy extends Component {
     this.subscription.remove();
   }
 
+  onToggleMessageQueue(value) {
+    MessageQueue.spy(value);
+    this.setState({ isMessageQueueOn: value });
+  }
+
+  onClose() {
+    this.setState({ modalVisible: false });
+  }
+
   render() {
-    return null;
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={this.state.modalVisible}
+        onRequestClose={() => this.setState({ modalVisible: false })}
+      >
+        <View style={styles.devMenuContainer}>
+          <Text style={styles.title}>Spy Menu</Text>
+          <View style={styles.rowStyle}>
+            <Text style={{ paddingTop: 5, marginHorizontal: 5 }}>
+              Message queue (React-Native bridge)
+            </Text>
+            <Switch
+              value={this.state.isMessageQueueOn}
+              onValueChange={this.onToggleMessageQueue}
+            />
+          </View>
+        </View>
+        <View style={styles.bottomContainer}>
+          <Button title="Close" onPress={this.onClose} />
+        </View>
+      </Modal>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 30
+  },
+  rowStyle: {
+    marginTop: 16,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  bottomContainer: {
+    backgroundColor: "skyblue",
+    paddingBottom: 30
+  },
+  devMenuContainer: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "skyblue"
+  }
+});
